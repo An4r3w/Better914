e5c41973cc4bf4ea97aaed861619c3e8a16a1315
An4r3w <75725607+An4r3w@users.noreply.github.com>
2023-05-29 16:52:13 +0200

    Add files via upload

---
+++ Better914/Config.cs
@@ -0,0 +1,29 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Interfaces;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Better914
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+
+        [Description("Decides wich effects should be applied to the player once it enters in an active 914 with specific knob setting")]
+        public List<EffectType> EffectsOnRough { get; set; } = new List<EffectType>() { EffectType.CardiacArrest, EffectType.Bleeding, };
+
+        public List<EffectType> EffectsOnCoarse { get; set; } = new List<EffectType> { EffectType.Hemorrhage, EffectType.Blinded };
+        public List<EffectType> EffectsOn11 { get; set; } = new List<EffectType> { EffectType.Poisoned, EffectType.Burned };
+        public List<EffectType> EffectsOnFine { get; set; } = new List<EffectType> { EffectType.Scp207, EffectType.Invigorated };
+        public List<EffectType> EffectsOnVeryFine { get; set; } = new List<EffectType> { EffectType.DamageReduction, EffectType.Flashed };
+
+        [Description("Decides the duration of the effects that are applied to the player once it enters in an active 914 with specific knob setting")]
+        public float EffectsOnRoughDuration { get; set; } = 15;
+
+        public float EffectsOnCoarseDuration { get; set; } = 15;
+        public float EffectsOn11Duration { get; set; } = 15;
+        public float EffectsOnFineDuration { get; set; } = 15;
+        public float EffectsOnVeryFineDuration { get; set; } = 15;
+    }
+}
---
+++ Better914/EventHandlers.cs
@@ -0,0 +1,32 @@
+﻿using Exiled.API.Enums;
+using Exiled.Events.EventArgs.Scp914;
+
+namespace Better914
+{
+    public class EventHandlers
+    {
+        public void OnUpgradingPlayer(UpgradingPlayerEventArgs ev)
+        {
+            if (ev.KnobSetting == Scp914.Scp914KnobSetting.Rough && Plugin.Singleton.Config.EffectsOnRough.Count != 0)
+            {
+                ev.Player.EnableEffects(Plugin.Singleton.Config.EffectsOnRough, duration: Plugin.Singleton.Config.EffectsOnRoughDuration);
+            }
+            else if (ev.KnobSetting == Scp914.Scp914KnobSetting.Coarse && Plugin.Singleton.Config.EffectsOnCoarse.Count != 0)
+            {
+                ev.Player.EnableEffects(Plugin.Singleton.Config.EffectsOnCoarse, duration: Plugin.Singleton.Config.EffectsOnCoarseDuration);
+            }
+            else if (ev.KnobSetting == Scp914.Scp914KnobSetting.OneToOne && Plugin.Singleton.Config.EffectsOn11.Count != 0)
+            {
+                ev.Player.EnableEffects(Plugin.Singleton.Config.EffectsOn11, duration: Plugin.Singleton.Config.EffectsOn11Duration);
+            }
+            else if (ev.KnobSetting == Scp914.Scp914KnobSetting.Fine && Plugin.Singleton.Config.EffectsOnFine.Count != 0)
+            {
+                ev.Player.EnableEffects(Plugin.Singleton.Config.EffectsOnFine, duration: Plugin.Singleton.Config.EffectsOnFineDuration);
+            }
+            else if (ev.KnobSetting == Scp914.Scp914KnobSetting.VeryFine && Plugin.Singleton.Config.EffectsOnVeryFine.Count != 0)
+            {
+                ev.Player.EnableEffects(Plugin.Singleton.Config.EffectsOnVeryFine, duration: Plugin.Singleton.Config.EffectsOnRoughDuration);
+            }
+        }
+    }
+}
